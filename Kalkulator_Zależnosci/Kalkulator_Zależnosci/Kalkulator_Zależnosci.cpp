ae21dfa096ad58fa1b9668b51498f89943b25ac2
JakubKiecok <jkkiecok@gmail.com>
2023-10-03 10:15:44 +0200

    Kalkulator

---
+++ Kalkulator_Zależnosci/Kalkulator_Zależnosci/Kalkulator_Zależnosci.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+float y1(float a);
+double y2(float a);
+float y3(float a, float b);
+void y4(float a, float b);
+
+int main()
+{   
+    setlocale(LC_ALL, "");
+    int menu;
+    float a, b;
+    cout << "Kalkulator zależności\n";
+    cout << "Y1 = 1, Y2 = 2, Y3 = 3, Y4 = 4" << endl; 
+    cout << "Podaj funkcje ktora chcesz wykonac = "; cin >> menu;
+
+
+    switch (menu)
+    {
+    case 1:
+        cout << "Podaj a = "; cin >> a;
+        cout << "a do potęgi 2 = " << y1(a) << endl;
+        break;
+    case 2:
+        cout << "Podaj a = "; cin >> a;
+        cout << "pierwiastek z liczby a = " << y2(a) << endl;
+        break;
+    case 3:
+        cout << "Podaj a = "; cin >> a;
+        cout << "Podaj b = "; cin >> b;
+        cout << "a + b / a * b = " << y3(a, b) << endl;
+        break;
+    case 4:
+        cout << "Podaj a = "; cin >> a;
+        cout << "Podaj b = "; cin >> b;
+        y4(a, b);
+        break;
+    default:
+        cout << "Nie wybrano opcji, zacznij od początku";
+        break;
+    }
+
+    system("pause");
+    return 0;
+}
+
+float y1(float a)
+{
+    return pow(a, 2);
+}
+
+double y2(float a)
+{
+    return sqrt(a);
+}
+
+float y3(float a, float b)
+{
+    return (a + b)/(a * b);
+}
+
+void y4(float a, float b)
+{
+    if (a != 0)
+    {
+        cout << "(a * b - b * a) / a = " <<(a * b - b * a) / a;
+    }
+    else
+    {
+        cout << "Nie dziel szlachcicu przez 0";
+    }
+}
---
+++ kalkulator_swich()_case/kalkulator_swich()_case/kalkulator_swich()_case.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+
+float dodawanie(float liczba1, float liczba2);
+float odejmowanie(float liczba1, float liczba2);
+float mnożenie(float liczba1, float liczba2);
+void dzielenie(float liczba1, float liczba2);
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int menu;
+    float liczba1, liczba2;
+    cout << "Kalkulator z funkcją swich() case\n";
+    cout << "Dodawanie = 1, Odejmowanie = 2, Mnożenie = 3. Dzielenie = 4" << endl;
+    cout << "Wybierz operacje = "; cin >> menu;
+    cout << "Podaj liczbę 1 = "; cin >> liczba1;
+    cout << "Podaj liczbę 2 = "; cin >> liczba2;
+
+    switch (menu)
+    {
+        case 1:
+            cout << "Dodawanie = " << dodawanie(liczba1, liczba2) << endl;
+            break;
+        case 2:
+            cout << "Odejmowanie = " << odejmowanie(liczba1, liczba2) << endl;
+            break;
+        case 3:
+            cout << "Mnożenie = " << mnożenie(liczba1, liczba2) << endl;
+            break;
+        case 4:
+            dzielenie(liczba1, liczba2);
+            break;
+    default:
+        cout << "Nie wybrano operacji: zacznij od początku" << endl;
+        break;
+    }
+
+    system("pause");
+    return 0;
+}
+
+float dodawanie(float liczba1, float liczba2)
+{
+    return liczba1 + liczba2;
+}
+
+float odejmowanie(float liczba1, float liczba2)
+{
+    return liczba1 - liczba2;
+}
+
+float mnożenie(float liczba1, float liczba2)
+{
+    return liczba1 * liczba2;
+}
+
+void dzielenie(float liczba1, float liczba2)
+{
+    if (liczba2 != 0) {
+        cout << "Dzielenie = " << liczba1 / liczba2 << endl;
+    }
+    else
+    {
+        cout << "Nie dziel szlachciu przez 0" << endl;
+    }
+}
